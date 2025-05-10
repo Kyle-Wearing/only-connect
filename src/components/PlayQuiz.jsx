d73201bb82d3f21987c5a888a5352df903597c3c
Kyle-Wearing <kylewearing@icloud.com>
2025-05-10 23:40:37 +0100

    set up glyph grid

--- src/components/PlayQuiz.jsx
+++ src/components/PlayQuiz.jsx
@@ -2,6 +2,12 @@ import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import "../styles/PlayQuiz.css";
 import { getAllQuestions } from "../../utils";
+import Lion from "../assets/Lion.png";
+import EyeOfHorus from "../assets/EyeOfHorus.png";
+import HornedViper from "../assets/HornedViper.png";
+import TwistedFlax from "../assets/TwistedFlax.png";
+import Tworeeds from "../assets/Tworeeds.png";
+import Water from "../assets/Water.png";
 
 export function PlayQuiz() {
   const { quiz_id } = useParams();
@@ -12,6 +18,15 @@ export function PlayQuiz() {
   const [team1Score, setTeam1Score] = useState(0);
   const [team2Score, setTeam2Score] = useState(0);
   const [questions, setQuestions] = useState({});
+  const [turn, setTurn] = useState(1);
+  const hieroglyphs = [
+    { name: "Lion", src: Lion },
+    { name: "Eye of Horus", src: EyeOfHorus },
+    { name: "Horned Viper", src: HornedViper },
+    { name: "Twisted Flax", src: TwistedFlax },
+    { name: "Two Reeds", src: Tworeeds },
+    { name: "Water", src: Water },
+  ];
 
   async function apiCall() {
     const fetchedQuestions = await getAllQuestions(quiz_id);
@@ -67,4 +82,15 @@ export function PlayQuiz() {
       </div>
     );
   }
+
+  return (
+    <div className="glyph-container">
+      {hieroglyphs.map((glyph) => (
+        <button key={glyph.name} className="glyph-tile">
+          <img src={glyph.src} alt={glyph.name} className="glyph-img" />
+          <p className="glyph-name">{glyph.name}</p>
+        </button>
+      ))}
+    </div>
+  );
 }
